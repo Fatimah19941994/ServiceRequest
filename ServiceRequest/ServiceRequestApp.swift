c92e1aba6cfdfadf78f58e6a9c78610b5332e14d
fatimah <fatimmaah1994@gmail.com>
2022-08-15 11:43:14 -0700

    Initial Commit

---
+++ ServiceRequest/ServiceRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceRequestApp.swift
+//  ServiceRequest
+//
+//  Created by فاطمة on 17/01/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ServiceRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
